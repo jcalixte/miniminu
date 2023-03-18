8c4911adf68a1fc8fb21f3e9663ed59e22152835
Julien Calixte <julienc@bam.tech>
2023-03-18 14:59:39 +0100

    sort imports

--- src/services/time-until.test.ts
+++ src/services/time-until.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, afterEach, beforeEach } from "vitest"
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 import { hasTimePassed, timeUntil } from "./time-until"
 
 const noTime = () => ({
